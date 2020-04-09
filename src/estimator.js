5da17bb3cae4f976ea1aff490755ddda10ac9583
manuelbiolatiri <manuellaide49@gmail.com>
2020-04-09 08:35:01 -0700

    assessment begins

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,36 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  if (!data) {
+    return null;
+  }
+
+  const reportedCases = data.reportedCases;
+  const data = {
+    region: {
+      name: "Africa",
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614,
+    impact: {
+      currentlyInfected:'reportedCases * 10',
+      infectionsByRequestedTime: 'currentlyInfected * 512'
+    },
+    severeImpact: {
+      currentlyInfected: 'reportedCases * 50',
+      infectionsByRequestedTime: 'currentlyInfected * 512'
+    }
+  }
+
+  return {
+          data: {},
+          impact: {},
+          severeImpact: {}
+        } 
+};
 
 export default covid19ImpactEstimator;
